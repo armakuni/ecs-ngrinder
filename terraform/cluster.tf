0136848e6bb36d551a3e8cadbdbf576ec2f6f42a
Billie Thompson <billie.thompson@armakuni.com>
2019-10-09 14:10:24 +0100

    Added ingress rule
    Add the ingress rule and security group to launch containers into.
    
    Co-authored-by: Tom Oram <tom.oram@armakuni.com>

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,4 +1,26 @@
+locals {
+  plaform_name = "${var.platform_name}-${random_id.platform_name_suffix.hex}"
+}
+
 resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "${var.platform_name}-${random_id.platform_name_suffix.hex}"
+  name = local.plaform_name
+}
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+resource "aws_security_group" "security_group" {
+  vpc_id = data.aws_vpc.default_vpc.id
+  name = local.plaform_name
+}
+
+resource "aws_security_group_rule" "http_ingress" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  to_port           = 80
+  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
 }
 
