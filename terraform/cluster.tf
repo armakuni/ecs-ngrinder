59a46af85e24631e117474b42fd4ed992651fd74
Tom Oram <tom.oram@armakuni.com>
2019-10-09 15:06:52 +0100

    Add task execution role

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -2,6 +2,31 @@ locals {
   plaform_name = "${var.platform_name}-${random_id.platform_name_suffix.hex}"
 }
 
+resource "aws_iam_role" "task_execution_role" {
+  name = local.plaform_name
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role_policy_attachement" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = local.plaform_name
 }
